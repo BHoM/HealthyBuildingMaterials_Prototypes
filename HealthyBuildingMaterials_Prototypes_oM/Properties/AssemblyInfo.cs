7c3a8ed8a374e0ccd6a66bc64b49d4f8ad683c5b
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2023-10-17 17:29:28 +0100

    Update for Michael

--- HealthyBuildingMaterials_Prototypes_oM/Properties/AssemblyInfo.cs
+++ HealthyBuildingMaterials_Prototypes_oM/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HealthyBuildingMaterials_Prototypes_oM")]
-[assembly: AssemblyDescription("https://github.com/BuroHappoldEngineering/HealthyBuildingMaterials_Prototypes")]
+[assembly: AssemblyDescription("https://github.com/BHoM/HealthyBuildingMaterials_Prototypes")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HealthyBuildingMaterials_Prototypes_oM")]
@@ -55,5 +55,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.3.0.0")]
+[assembly: AssemblyVersion("7.0.0.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
